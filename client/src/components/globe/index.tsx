5b2c9904c01326d024580fc528e31a3af4683ff4
Tamsen1995 <tamanhbui95@gmail.com>
2024-04-01 15:05:39 -0400

    Add geolocations and color change functionality to Globe component

--- client/src/components/globe/index.tsx
+++ client/src/components/globe/index.tsx
@@ -6,6 +6,7 @@ const Globe = dynamic(() => import("react-globe.gl"), { ssr: false });
 
 const MyGlobe = () => {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [color, setColor] = useState("red");
 
   useEffect(() => {
     setDimensions({
@@ -14,13 +15,41 @@ const MyGlobe = () => {
     });
   }, []);
 
+  // Define your geolocations here
+  const geolocations = [
+    { lat: 51.5074, lng: 0.1278 }, // London
+    { lat: 40.7128, lng: -74.006 }, // New York
+    // Add more geolocations as needed
+  ];
+
+  // Convert geolocations to pointsData
+  const pointsData = geolocations.map((location, index) => ({
+    lat: location.lat,
+    lng: location.lng,
+    color: color,
+    altitude: 0.1,
+    radius: 0.5,
+  }));
+
+  const handleClick = () => {
+    console.log("Clicked!");
+    // Change color on click
+    setColor(color === "red" ? "blue" : "red");
+  };
+
   return (
-    <Globe
-      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-      backgroundColor="rgba(0,0,0,1)" // Set to black
-      width={dimensions.width}
-      height={dimensions.height}
-    />
+    <div onClick={handleClick}>
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        backgroundColor="rgba(0,0,0,1)" // Set to black
+        width={dimensions.width}
+        height={dimensions.height}
+        pointsData={pointsData}
+        pointAltitude="altitude"
+        pointColor="color"
+        pointRadius="radius"
+      />
+    </div>
   );
 };
 
