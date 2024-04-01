cfb95fcbae04cc34117aa95b4d2b1546ec5556a8
Tamsen1995 <tamanhbui95@gmail.com>
2024-04-01 10:37:26 -0400

    Add mapbox-gl and @types/mapbox-gl dependencies, and update globe component to use mapbox-gl

--- client/src/components/globe/index.tsx
+++ client/src/components/globe/index.tsx
@@ -1,32 +1,46 @@
-import React from "react";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
-import { Sphere, OrbitControls } from "@react-three/drei";
+import React, { useEffect, useRef } from "react";
+import mapboxgl, { Projection } from "mapbox-gl";
 
-const Earth = () => {
-  const texture = useLoader(TextureLoader, "./globe.jpg");
-
-  if (!texture) {
-    return null; // or return a loading indicator
-  }
-
-  return (
-    <Sphere args={[1, 32, 32]}>
-      <meshStandardMaterial map={texture} />
-    </Sphere>
-  );
-};
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidGJ1aTE5OTUiLCJhIjoiY2x1Z3llbGhtMmtrajJtbGRzd3ZjbXhpeiJ9.PydtIujr8rhy0cBttOI-IQ";
 
 const Globe: React.FC = () => {
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (mapContainerRef.current) {
+      const map = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: "mapbox://styles/mapbox/dark-v10", // Changed to dark-v10 for a night theme
+        center: [0, 0],
+        zoom: 1,
+        pitch: 60, // pitch in degrees
+        bearing: -60, // bearing in degrees
+        projection: "globe" as unknown as Projection, // Fixed the type error by casting the value to Projection type
+      });
+
+      const nav = new mapboxgl.NavigationControl();
+      map.addControl(nav, "top-right");
+
+      map.on("load", () => {
+        const mapStyle = map.getStyle();
+        if (mapStyle) {
+          mapStyle.light = {
+            anchor: "viewport",
+            color: "#ffffff",
+            intensity: 0.1,
+          };
+          map.setStyle(mapStyle);
+        }
+      });
+
+      return () => map.remove();
+    }
+  }, []);
+
   return (
-    <div className="flex items-center justify-center h-screen flex-col">
-      <Canvas style={{ background: "#DC143C" }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <Earth />
-        <OrbitControls />
-      </Canvas>
-    </div>
+    <div ref={mapContainerRef} style={{ width: "100%", height: "100vh" }} />
   );
 };
+
 export default Globe;
